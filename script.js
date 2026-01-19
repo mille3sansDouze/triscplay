88fb95cc8f88e6835a2154b0e678f2acf351d1d1
mill3 <alphee.sordetsalin@viacesi.fr>
2026-01-19 11:45:25 +0100

    adding js for admin page, pages are now interconnected, a lot of changes the website is now ready for hosting

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const hello = document.getElementById("home-hello")
 const btn = document.getElementById("home-hello-button")
 const del = document.getElementById("home-hello-del")
 
+
 function login() {
     const name = prompt("Entrez votre blaze")
     if (!name) return
@@ -11,6 +12,20 @@ function login() {
     hello.textContent = "Bienvenue " + name + " !"
 
     btn.remove();
+    if(name === "admin") {
+        
+        console.log("welcome admin :3")
+
+        const adminbtn = document.createElement("button")
+        adminbtn.classList.add("btnadmin")
+
+        const aadminbtn = document.createElement("a")
+        aadminbtn.textContent = "Admin Panel"
+        aadminbtn.setAttribute("href", "https://youtu.be/dQw4w9WgXcQ?si=sptKy3dsGIbUCVnz")
+
+        adminbtn.appendChild(aadminbtn)
+        document.body.appendChild(adminbtn)
+    }
 }
 
 // bouton supprimer
@@ -27,8 +42,26 @@ const name = localStorage.getItem("name")
 if (register === "y" && name) {
     hello.textContent = "Bienvenue " + name + " !"
     btn.remove();
+
+    if(name === "admin"){
+
+        console.log("welcome admin :3")
+
+        const adminbtn = document.createElement("button")
+        adminbtn.classList.add("btnadmin")
+
+        const aadminbtn = document.createElement("a")
+        aadminbtn.textContent = "Admin Panel"
+        aadminbtn.setAttribute("href", "https://youtu.be/dQw4w9WgXcQ?si=sptKy3dsGIbUCVnz")
+
+        adminbtn.appendChild(aadminbtn)
+        document.body.appendChild(adminbtn)
+        
+    }
 }else {
     hello.textContent = ""
 }
 
 btn.addEventListener("click", login)
+
+
