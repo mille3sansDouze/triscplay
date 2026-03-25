af61247c152eaa92b6d3001bedd0643ad518f78a
alphee sordet salin <alphee.sordet1312@gmail.com>
2026-03-25 16:58:39 +0100

    updating the addgamepage and adminpage, adding a form for add a game, updating css for those pages

--- scripts/addgame.js
+++ scripts/addgame.js
@@ -1,5 +1,4 @@
 const addbtn = document.getElementById("get")
-const postbtn = document.getElementById("post")
 const delbtn = document.getElementById("del")
 /*const addbtn = document.getElementById("get")*/
 
@@ -7,12 +6,14 @@ const delbtn = document.getElementById("del")
 function renderData(data) {
   const container = document.getElementById("reponse");
     container.innerHTML = data.map(game => `
-      <div class="card">
+    <div class="card">
+      <div class="card-inner">
         <h2>${game.name}</h2>
         <p>${game.description}</p>
         <p class="meta">PEGI : ${game.pegi} | Joueurs : ${game.playerCount} | Multi : ${game.isMultiplayer ? "Oui" : "Non"}</p>
         <small>ID : ${game.id}</small>
       </div>
+    </div>
     `).join("");
 
 }
@@ -31,26 +32,65 @@ async function loadGames() {
 /*=============================*/
 
 async function postGame() {
-  let name = prompt("nom");
-  let description = prompt("description");
+  event.preventDefault()
+  let name = document.getElementById("gameName").value
+  if (!name || typeof name !== "string" || name.length === 0) {
+    alert("Le nom est invalide.")
+    return
+  }
+  let description = document.getElementById("gameDescription").value
+  if (!description || typeof description !== "string" || description.length === 0) {
+    alert("La description est invalide.")
+    return
+  }
+  let playercount = document.getElementById("gamePlayerCount").value
+  if (isNaN(playercount) || playercount < 1) {
+    alert("Le nombre de joueurs est invalide.")
+    return
+  }
+  let pegi = document.getElementById("gamePegi").value
+  if (isNaN(pegi) || pegi < 0) {
+    alert("Le PEGI est invalide.")
+    return
+  }
+  let isMultiplayer = document.getElementById("gameMultiplayer").value
+  if (isMultiplayer !== "true" && isMultiplayer !== "false") {
+    alert("Veuillez sélectionner une option pour le multijoueur.")
+    return
+  }
 
-  const res = await fetch("http://localhost:3000/game", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      name: name,
-      description: description,
-      pegi: 18,
-      playerCount: 1,
-      isMultiplayer: false
-    })
-  });
+  document.getElementById("gameName").value = "";
+  document.getElementById("gameDescription").value = "";
+  document.getElementById("gamePlayerCount").value = "";
+  document.getElementById("gamePegi").value = "";
+  document.getElementById("gameMultiplayer").value = "";
+  
 
-  const data = await res.json();
-  console.log(data);
-  await loadGames()
+  try {
+    const res = await fetch("http://localhost:3000/game", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: name,
+        description: description,
+        pegi: pegi,
+        playerCount: playercount,
+        isMultiplayer: false
+      })
+    });
+
+    const data = await res.json();
+    console.log(data);
+    await loadGames() 
+  }
+  catch {
+    alert("sa marche pas zebi")
+  }
+
+
+
 }
 
 /*=============================*/
@@ -80,12 +120,10 @@ addbtn.addEventListener("click", () => {
     
 })
 
-postbtn.addEventListener("click", () =>{
-    postGame();
-})
-
 delbtn.addEventListener("click", () => {
     const id = prompt("id del")
     delGame(id);
 
 })
+
+
